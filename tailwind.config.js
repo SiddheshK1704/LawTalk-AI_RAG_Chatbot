14b141c971dad532ec64654ae6a9310cac3df070
SiddheshK1704 <siddheshkhankhoje@gmail.com>
2026-03-23 23:13:04 +0530

    Restructured frontend and updated gitignore

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./dist/**/*.html"], // This will correctly match all HTML files in the dist folder and its subdirectories
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
