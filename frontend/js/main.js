b1e40f7bd5893beea3049a0fcc78d1aea58db3c1
SiddheshK1704 <siddheshkhankhoje@gmail.com>
2026-03-31 21:14:10 +0530

    Frontend tested, and set up.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,9 @@
-import "./ui.js";
+window.onload = () => {
+  const app = document.getElementById("app");
 
-console.log("JS Loaded");
+  app.innerHTML = `
+    <div class="h-screen flex items-center justify-center bg-black text-white">
+      <h1 class="text-4xl font-bold">LawTalk is Working 🚀</h1>
+    </div>
+  `;
+};
